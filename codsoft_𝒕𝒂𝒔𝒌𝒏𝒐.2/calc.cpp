4868260dd7750b8aec393a8c0a44ddf3442b69a3
HASEEB UR REHMAN <92214860+mehaseeburrehman@users.noreply.github.com>
2023-09-04 20:41:01 +0500

    Delete codsoft_𝒕𝒂𝒔𝒌𝒏𝒐.2 directory

--- codsoft_𝒕𝒂𝒔𝒌𝒏𝒐.2/calc.cpp
+++
@@ -1,7 +0,0 @@
-#include"calc.h"
-#include<iostream>
-#include<math.h>
-double Calculator::add(double a, double b){ return a + b; }
-double Calculator::mul(double a, double b){ return a*b; }
-double Calculator::div(double a, double b){ return a / b; }
-double Calculator::sub(double a, double b){ return a - b; }
